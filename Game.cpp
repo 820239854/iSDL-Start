6347f03a7095cc99b2834f502938180f8f25f463
Archer <820239854@qq.com>
2023-04-23 19:50:05 +0800

    Render Sprite Sheet

--- Game.cpp
+++ Game.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "Game.h"
 
-bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+bool Game::init(const char *title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	// attempt to initialize SDL
-	if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
 		int flags = 0;
-		if(fullscreen)
+		if (fullscreen)
 		{
 			flags = SDL_WINDOW_FULLSCREEN;
 		}
@@ -16,12 +16,12 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		// init the window
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 
-		if(m_pWindow != 0) // window init success
+		if (m_pWindow != 0) // window init success
 		{
 			std::cout << "window creation success\n";
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
-			if(m_pRenderer != 0) // renderer init success
+			if (m_pRenderer != 0) // renderer init success
 			{
 				std::cout << "renderer creation success\n";
 				SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
@@ -47,15 +47,14 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	std::cout << "init success\n";
 	m_bRunning = true; // everything inited successfully, start the main loop
 
-	SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+	SDL_Surface *pTempSurface = SDL_LoadBMP("assets/animate.bmp");
 	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 	SDL_FreeSurface(pTempSurface);
-	SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
 
 	m_destinationRectangle.x = m_sourceRectangle.x = 0;
 	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
+	m_destinationRectangle.w = m_sourceRectangle.w = 128;
+	m_destinationRectangle.h = m_sourceRectangle.h = 82;
 
 	return true;
 }
@@ -64,7 +63,8 @@ void Game::render()
 {
 	SDL_RenderClear(m_pRenderer); // clear the renderer to the draw color
 	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
-	SDL_RenderPresent(m_pRenderer); // draw to the screen
+	// SDL_RenderCopyEx(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle, 0, 0, SDL_FLIP_HORIZONTAL); // pass in the horizontal flip
+	SDL_RenderPresent(m_pRenderer);																					   // draw to the screen
 }
 
 void Game::clean()
@@ -78,16 +78,21 @@ void Game::clean()
 void Game::handleEvents()
 {
 	SDL_Event event;
-	if(SDL_PollEvent(&event))
+	if (SDL_PollEvent(&event))
 	{
-		switch(event.type)
+		switch (event.type)
 		{
-			case SDL_QUIT:
-				m_bRunning = false;
+		case SDL_QUIT:
+			m_bRunning = false;
 			break;
 
-			default:
+		default:
 			break;
 		}
 	}
+}
+
+void Game::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
