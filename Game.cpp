8a0107a07838f6100f942d5d6be6277f80dd4400
Archer <820239854@qq.com>
2023-04-23 19:57:27 +0800

    Init GameObject

--- Game.cpp
+++ Game.cpp
@@ -2,13 +2,13 @@
 #include "Game.h"
 #include "TextureManager.h"
 
-bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+bool Game::init(const char *title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	// attempt to initialize SDL
-	if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
 		int flags = 0;
-		if(fullscreen)
+		if (fullscreen)
 		{
 			flags = SDL_WINDOW_FULLSCREEN;
 		}
@@ -17,12 +17,12 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		// init the window
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 
-		if(m_pWindow != 0) // window init success
+		if (m_pWindow != 0) // window init success
 		{
 			std::cout << "window creation success\n";
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
-			if(m_pRenderer != 0) // renderer init success
+			if (m_pRenderer != 0) // renderer init success
 			{
 				std::cout << "renderer creation success\n";
 				SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
@@ -48,10 +48,13 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	std::cout << "init success\n";
 	m_bRunning = true; // everything inited successfully, start the main loop
 
-	if(!TheTextureManager::Instance()->load("assets/animate.jpg", "animate", m_pRenderer))
+	if (!TheTextureManager::Instance()->load("assets/animate.jpg", "animate", m_pRenderer))
 	{
 		return false;
 	}
+
+	m_go.load(100, 100, 128, 82, "animate");
+	m_player.load(300, 300, 128, 82, "animate");
 
 	return true;
 }
@@ -59,10 +62,9 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 void Game::render()
 {
 	SDL_RenderClear(m_pRenderer);
-	
 
-	TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82, m_pRenderer);
-	TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
+	m_go.draw(m_pRenderer);
+	m_player.draw(m_pRenderer);
 
 	SDL_RenderPresent(m_pRenderer);
 }
@@ -78,15 +80,15 @@ void Game::clean()
 void Game::handleEvents()
 {
 	SDL_Event event;
-	if(SDL_PollEvent(&event))
+	if (SDL_PollEvent(&event))
 	{
-		switch(event.type)
+		switch (event.type)
 		{
-			case SDL_QUIT:
-				m_bRunning = false;
+		case SDL_QUIT:
+			m_bRunning = false;
 			break;
 
-			default:
+		default:
 			break;
 		}
 	}
@@ -94,5 +96,6 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+	m_go.update();
+	m_player.update();
 }
